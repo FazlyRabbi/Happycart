386c1d27cf00debd731b9e7789ee7c610b31af6c
hr-meheraj <hrmeheraj2021@gmail.com>
2022-08-08 19:41:29 +0600

    single product changed

--- src/pages/Home/SingleProduct/ProductSection.jsx
+++ src/pages/Home/SingleProduct/ProductSection.jsx
@@ -44,31 +44,37 @@ function ProductSection() {
                 </div>
                 {/* Show more */}
                 <div className={`${show ? "" : "hidden"}`}>
-                <div className="flex gap-2 mb-3">
-                    <p className='font-bold'>Printing Technology</p>
-                    <p>Inkjet</p>
+                    <div className="flex gap-2 mb-3">
+                        <p className='font-bold'>Printing Technology</p>
+                        <p>Inkjet</p>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <p className='font-bold'>Printing Technology</p>
+                        <p>Inkjet</p>
+                    </div>
+                    <div className="flex gap-2 mb-3">
+                        <p className='font-bold'>Printing Technology</p>
+                        <p>Inkjet</p>
+                    </div>
                 </div>
-                <div className="flex gap-2 mb-3">
-                    <p className='font-bold'>Printing Technology</p>
-                    <p>Inkjet</p>
-                </div>
-                <div className="flex gap-2 mb-3">
-                    <p className='font-bold'>Printing Technology</p>
-                    <p>Inkjet</p>
-                </div>
-                </div>
-                <button className="bg-blue-500 text-white rounded-md py-2 px-4" onClick={() => setShow(!show)}>Show More</button>
+                <button className="bg-blue-500 text-white rounded-md py-2 px-4" onClick={() => setShow(!show)}>{show ? "Hide" : "Show More"}</button>
             </div>
-            <div className=" p-4 border-2 border-gray-800 rounded-md col-span-3 m-4  md:col-span-1">
-                <p>Buy New : <span className='text-red-800'>Tk. 10000</span></p>
-                <br/>
-                <p className='mb-2'>Tk. 100 delivery <span className='font-bold'>July 12 - August 2.</span></p>
-                <p className='mb-3'>Delivery to <span className='font-bold'>Dhaka</span><span className='text-[18px'>{"(Change)"}</span></p>
-                <p>Solve by <span className='font-bold text-blue-400'>HP</span></p>
-                <br/>
-                <div className='flex justify-center'>
+            <div className="col-span-3 m-4  md:col-span-1">
+                <div className='p-4 border-2 border-gray-800 rounded-md '>
+                    <p>Buy New : <span className='text-red-800'>Tk. 10000</span></p>
+                    <br />
+                    <p className='mb-2'>Tk. 100 delivery <span className='font-bold'>July 12 - August 2.</span></p>
+                    <p className='mb-3'>Delivery to <span className='font-bold'>Dhaka</span><span className='text-[18px'>{"(Change)"}</span></p>
+                    <p>Solve by <span className='font-bold text-blue-400'>HP</span></p>
+                    <br />
+                    <div className='flex justify-center'>
                         <button className='bg-yellow-400 w-8/12 text-black rounded-full py-2 px-4'>Add to Cart</button>
-                        <br/>
+                        <br />
+                    </div>
+                </div>
+                <br />
+                <div className='border-2 border-gray-800 p-4 rounded-md flex justify-center'>
+                    <button className='py-3 px-4 bg-gray-100 border-2 border-gray-500 w-9/12 rounded-md'>Add to Cart List</button>
                 </div>
             </div>
         </div>
