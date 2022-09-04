7c2342581b6085fe64d8ade84a3e59ca2f8182a0
absiddikhp <absiddikhp@gmail.com>
2022-09-04 18:55:30 +0600

    swiper slider bug fix

--- src/pages/SinglePage/RelatedProducts.jsx
+++ src/pages/SinglePage/RelatedProducts.jsx
@@ -1,55 +1,12 @@
+import React from 'react'
+import SwiperSliderCardBox from '../../shared/SwiperSliderCardBox'
 
-import Slider from "react-slick";
-import ProductCard from "./../../shared/ProductCard/ProductCard";
-const RelatedProducts = ({ products }) => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    arrows: false,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 5,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-    ],
-  };
+const RelatedProducts = ({products}) => {
   return (
-    <div className="p-4 lg:p-10 max-h-max">
-      <Slider {...settings}>
-        {products.map((product, index) => {
-          return (
-            <div key={index} className="mx-2 p-2 ">
-              <ProductCard data={product} />
-            </div>
-          );
-        })}
-      </Slider>
+    <div>
+      <SwiperSliderCardBox products={products} responsive={[2,2,4,5]} />
     </div>
-  );
-};
+  )
+}
 
-export default RelatedProducts;
-
-
+export default RelatedProducts
